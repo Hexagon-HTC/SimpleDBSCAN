ca629ead62e750825851b157789bb9b4afb643ea
Jakub Sandomierz <jakub.sandomierz@deepsense.ai>
2023-02-01 13:39:30 +0100

    Fixed creating package for conan

--- conanfile.py
+++ conanfile.py
@@ -1,3 +1,4 @@
+import platform
 from conans import ConanFile, CMake, tools
 from conan.tools.cmake import CMakeToolchain, CMake
 
@@ -12,6 +13,7 @@ class SimpleDBSCANConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
     exports_sources = ["CMakeLists.txt", "src/*", "include/*", "FindSimpleDBSCAN.cmake"]
+    generators = "cmake_find_package", "cmake_paths"
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -22,22 +24,19 @@ class SimpleDBSCANConan(ConanFile):
         cmake.configure()
         cmake.build()
 
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
     def package(self):
-        cmake = CMake(self)
-        cmake.install()
-
         self.copy("*.h")
+        self.copy("*.hpp")
         self.copy("*.dll", keep_path=True)
         self.copy("*.so", keep_path=True)
         self.copy("*.dylib", keep_path=True)
         self.copy("*.a", keep_path=True)
-
+        self.copy("*.lib", keep_path=True)
 
     def package_info(self):
+        if platform.system() == "Windows":
+            if self.settings.build_type == "Debug":
+                self.cpp_info.libdirs = ['lib/Debug']
+            else:
+                self.cpp_info.libdirs = ['lib/Release']
         self.cpp_info.libs = ["dbscan"]
-
